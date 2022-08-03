e7aef096694b5e6ee68908ac3adcf7968ea38d99
Kasey Jones <kaseyjones49@gmail.com>
2022-08-03 16:44:54 -0500

    Moved music folder to the correct level + edited views file to the correct syntax for @api_view requests to be set up

---
+++ music/views.py
@@ -0,0 +1,47 @@
+from .models import Song
+from .serializers import SongSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+
+
+# Create your views here.
+
+@api_view(['GET', 'POST'])
+def songs_list (request):
+
+    if request.method == 'GET':
+        songs = Song.objects.all()
+        serializer = SongSerializer(songs, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+        
+    elif request.method == 'POST':
+        serializer = SongSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=400)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def songs_detail(request, id):
+        songs = get_object_or_404(Song, pk=id)
+        if request.method == 'GET':
+            serializer = SongSerializer(songs)
+            return Response(serializer.data)
+
+        elif request.method == 'PUT':
+            serializer = SongSerializer(songs, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        elif request.method == 'DELETE':
+            songs.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
